760999e251144d947f45d0b42e5c99aa37bfab0d
Mathieu <mathieu.linder.dev@gmail.com>
2021-11-21 05:39:30 +0100

    starter files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+export default App;
